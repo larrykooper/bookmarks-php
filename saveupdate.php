35112694e6b57c0a1cbf485f9c1519f3fda68da3
Larry Kooper <larry.kooper@gmail.com>
2015-01-06 16:51:28 -0500

    Cleanup of trailing spaces

--- saveupdate.php
+++ saveupdate.php
@@ -5,7 +5,7 @@ require_once('db_con.php');
 $Deleting = 0;
 $frmPrivate = "n";
 $frmInRotation = "y";
-if ($_POST['submit'] == "save") 
+if ($_POST['submit'] == "save")
 {
     $Mode = "save";
 }
@@ -15,26 +15,26 @@ elseif ($_POST['submit'] == "delete")
     $Deleting = 1;
     $SiteToDelete = $_POST['siteediting'];
 }
-else 
+else
 {
     $Mode = "notsave";
-} 
+}
 
 $frmNewPage = $_POST['redirectURL'];
 
 If ($Deleting)
 {
-    $myDelStmt1 = "DELETE FROM UserSiteTag WHERE URLID =" . $SiteToDelete . " AND UserID='". $theusername ."'";     
-    $result6 = mysql_query($myDelStmt1) or die (mysql_error()."<br />Couldn't execute query: $myDelStmt1"); 
+    $myDelStmt1 = "DELETE FROM UserSiteTag WHERE URLID =" . $SiteToDelete . " AND UserID='". $theusername ."'";
+    $result6 = mysql_query($myDelStmt1) or die (mysql_error()."<br />Couldn't execute query: $myDelStmt1");
     $myDelStmt2 = "DELETE FROM UserSite WHERE URLID =" . $SiteToDelete . " AND UserID ='" . $theusername ."'";
-    $result6 = mysql_query($myDelStmt2) or die (mysql_error()."<br />Couldn't execute query: $myDelStmt2"); 
+    $result6 = mysql_query($myDelStmt2) or die (mysql_error()."<br />Couldn't execute query: $myDelStmt2");
 }  // end if deleting
 
 // Now save stuff if we need to -- delete this code
-if ($Mode == "save") 
-{   
+if ($Mode == "save")
+{
     // Get data from form
-    $frmDescr = $_POST['description'];  
+    $frmDescr = $_POST['description'];
     $frmDescr = addslashes($frmDescr);
     $frmExtended = $_POST['extended'];
     $frmTagString = $_POST['tags'];
@@ -44,50 +44,51 @@ if ($Mode == "save")
     }
     if (array_key_exists('cb_private', $_POST)) {
         $frmPrivate = $_POST['cb_private'];
-    }    
-    $OldTags = $_POST['oldtagstring'];  
-    
-    if ($frmInRotation == "y") 
+    }
+    $OldTags = $_POST['oldtagstring'];
+
+    if ($frmInRotation == "y") {
         $dbInro = 1;
-    else 
+    } else {
         $dbInro = 0;
-    if ($frmPrivate == "y")
+    }
+    if ($frmPrivate == "y") {
         $dbPriv = 1;
-    else
-        $dbPriv = 0;    
-    
+    } else {
+        $dbPriv = 0;
+    }
     // Update UserSite table
-    $myUpStmt = "UPDATE UserSite SET SiteDescr = '" . $frmDescr . "', ExtendedDesc = '" . $frmExtended . "', InRotation = " . $dbInro . ", Private = ". $dbPriv . " WHERE URLID = " . $frmSiteEditing . " AND UserID = '$theusername'"; 
-    $result5 = mysql_query($myUpStmt) or die (mysql_error()."<br />Couldn't execute query: $myUpStmt"); 
-    // Update tags if we need to    
-    if ($frmTagString <> $OldTags) 
-    { 
-        $myDelStmt = "DELETE FROM UserSiteTag WHERE URLID =" . $frmSiteEditing . " AND UserID='". $theusername ."'";        
-        $result6 = mysql_query($myDelStmt) or die (mysql_error()."<br />Couldn't execute query: $myDelStmt");           
-        //  parse out the tags and write them to db              
+    $myUpStmt = "UPDATE UserSite SET SiteDescr = '" . $frmDescr . "', ExtendedDesc = '" . $frmExtended . "', InRotation = " . $dbInro . ", Private = ". $dbPriv . " WHERE URLID = " . $frmSiteEditing . " AND UserID = '$theusername'";
+    $result5 = mysql_query($myUpStmt) or die (mysql_error()."<br />Couldn't execute query: $myUpStmt");
+    // Update tags if we need to
+    if ($frmTagString <> $OldTags)
+    {
+        $myDelStmt = "DELETE FROM UserSiteTag WHERE URLID =" . $frmSiteEditing . " AND UserID='". $theusername ."'";
+        $result6 = mysql_query($myDelStmt) or die (mysql_error()."<br />Couldn't execute query: $myDelStmt");
+        //  parse out the tags and write them to db
         $MyArray = explode(' ',$frmTagString);
         $tagcount = 1;
         foreach ($MyArray as $theTag)
-        {   
+        {
             If (strlen($theTag) > 0)
-            {       
-                $queryCheckDup = "SELECT Tag FROM UserSiteTag WHERE UserID = '$theusername' AND URLID = $frmSiteEditing 
+            {
+                $queryCheckDup = "SELECT Tag FROM UserSiteTag WHERE UserID = '$theusername' AND URLID = $frmSiteEditing
 AND Tag = '$theTag'";
-                $resultD = mysql_query($queryCheckDup) or die (mysql_error()."<br />Couldn't execute query: $queryCheckDup");   
-                $my_num = mysql_num_rows($resultD); 
-                if ($my_num == 0) 
+                $resultD = mysql_query($queryCheckDup) or die (mysql_error()."<br />Couldn't execute query: $queryCheckDup");
+                $my_num = mysql_num_rows($resultD);
+                if ($my_num == 0)
                 {
-                    // do the insert                
-                    $myInsStmt = "INSERT INTO UserSiteTag (UserID, URLID, Tag, TagOrder) VALUES ('" . $theusername ."',". $frmSiteEditing ." , '". $theTag . "', ". $tagcount .")";             
-                    $result7 = mysql_query($myInsStmt) or die (mysql_error()."<br />Couldn't execute query: $myInsStmt");       
-                    $tagcount = $tagcount + 1;  
-                }               
-            }               
-        }  // ends foreach loop      
-    }   // if new tags differ from old tags         
+                    // do the insert
+                    $myInsStmt = "INSERT INTO UserSiteTag (UserID, URLID, Tag, TagOrder) VALUES ('" . $theusername ."',". $frmSiteEditing ." , '". $theTag . "', ". $tagcount .")";
+                    $result7 = mysql_query($myInsStmt) or die (mysql_error()."<br />Couldn't execute query: $myInsStmt");
+                    $tagcount = $tagcount + 1;
+                }
+            }
+        }  // ends foreach loop
+    }   // if new tags differ from old tags
 } // if mode = save
 
 // End of saving stuff
 header('Location: '.$frmNewPage);
-die();  
+die();
 ?>
