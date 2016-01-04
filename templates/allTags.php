0cf965c3f0f4226e7a7b04ac294a8bdd08715fed
larrykooper <larry.kooper@gmail.com>
2016-01-04 11:19:01 -0500

    Added HTML to allTags

--- templates/allTags.php
+++ templates/allTags.php
@@ -1,15 +1,40 @@
 
-
-<? foreach ($tags_data as $tag_row): ?>
-
-    <? extract($tag_row); ?>
-
-    <!-- do this for each tag -->
-    <div class=LBBTag>
-        <span class="LBBNum"><?=$tcount ?></span>
-        <a href="bookmarks.php?tags=<?= $Tag ?>&amp;user=<?=$userdisp ?>" class="right">
-            <?= $Tag ?>
-        </a>
+<div class='LBBRight'>
+    <div class='LBBRightTitle'>
+        All tags
     </div>
 
-<? endforeach ?>
+    <? foreach ($tags_data as $tag_row): ?>
+
+        <? extract($tag_row); ?>
+
+        <!-- do this for each tag -->
+        <div class=LBBTag>
+            <span class="LBBNum"><?=$tcount ?></span>
+            <a href="bookmarks.php?tags=<?= $Tag ?>&amp;user=<?=$userdisp ?>" class="right">
+                <?= $Tag ?>
+            </a>
+        </div>
+
+    <? endforeach ?>
+
+    <!-- tag-sorting options -->
+
+    <div class='LBBRightTitle'>
+        sort by
+        <? if ($_SESSION['tagsort']=='freq'): ?>
+            <a class="right" href="bookmarks.php?user="<?= $userdisp ?>"&amp;settagsort=alpha">
+                alpha
+            </a>
+             | freq
+        <? else: ?>
+            alpha
+            <a class="right" href="bookmarks.php?user="<?= $userdisp ?>"&amp;settagsort=freq">
+                | freq
+            </a>
+
+        <? endif ?>
+
+    </div>
+
+</div>
